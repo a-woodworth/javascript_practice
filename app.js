524b528170c029b4d8f76389d4e8983e8bf665c2
Amy Woodworth <woodworthcommunications@gmail.com>
2016-02-05 14:01:33 -0600

    Add variables to be able to create a new task list item

--- app.js
+++ app.js
@@ -6,17 +6,34 @@ var addButton = document.getElementsByTagName("button")[0]; //first button
 var incompleteTasksHolder = document.getElementById("incomplete-tasks"); //incomplete-tasks
 var completedTasksHolder = document.getElementById("completed-tasks"); //completed-tasks
 
+//New Task List Item
+var createNewTaskElement = function(taskString) {
+  //Create List Item
+  var ListItem = document.createElement("li");
+  //input (checkbox)
+  var checkBox = document.createElement("input"); //checkbox
+  //label
+  var label = document.createElement("label");
+  //input (text)
+  var editInput = document.createElement("input"); //text
+  //button.edit
+  var editButton = document.createElement("button");
+  //button.delete
+  var deleteButton = document.createElement("button");
+
+    //Each element needs modifying
+    //Each element needs appending
+  return listItem;
+}
+
 //Add a new task
 var addTask = function() {
   console.log("Add task...");
-  //When button is pressed
   //Create a new list item with the text from the #new-task:
-  //input (checkbox)
-  //label
-  //input (text)
-  //button.edit
-  //button.delete
-  //Each element needs to be modified and appended
+  var ListItem = createNewTaskElement("Some new Task");
+
+  //Append listItem to incompleteTasksHolder
+
 }
 
 //Edit existing task
